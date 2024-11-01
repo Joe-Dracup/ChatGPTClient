bea5fef79aa3dafa3a0db213f6c49d1cac16e9cb
thomas.finchett <thomas.finchett@ignite.systems>
2024-11-01 13:22:44 +0000

    added formkits config themes and page

---
+++ client/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <FormKit 
+    type="text" 
+    name="textbox" 
+    id="textbox" 
+    label=" What do you need help with?"
+    />
+    <FormKit
+    type="button" 
+    name="search" 
+    id="Search" 
+    label=" Click to search"
+    />
+
+</template>
